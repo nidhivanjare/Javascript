0f07612e5bfe98c7be403b0f217f3351c7f32855
nidhivanjare <rnidhivanjare@gmail.com>
2021-02-24 23:18:12 +0530

    termial execution

--- Rock_paper_Scissors/game.js
+++ Rock_paper_Scissors/game.js
@@ -1,24 +1,61 @@
+const readline = require('readline-sync');
 
-var final_score = 0
+var player_score = 0
+var computer_score = 0
+b = false
+
+
+var playerSelection
 
 function playRound(playerSelection, computerSelection ) {
 
-  if(playerSelection == 'rock' && computerSelection == 'rock')
-  score = 0
-  else if(playerSelection == 'scissor' && computerSelection == 'rock')
-  score = 0
-  else if(playerSelection == 'rock' && computerSelection == 'paper')
-  score = 0
-  else if(playerSelection == 'scissor' && computerSelection == 'scissor')
-  score = 0
-  else if(playerSelection == 'scissor' && computerSelection == 'rock')
-  score = 0
-  else if(playerSelection == 'paper' && computerSelection == 'scissor')
-  score = 0
-  else
-  score = 1
+ if(playerSelection == computerSelection)
+ {
+    score = 0
+    com_score = 0
+ }
+ else if(playerSelection == 'rock')
+ {
+     if(computerSelection == 'scissor')
+     {
+        score = 1
+        com_score = 0
+     }
+     else if(computerSelection == 'paper')
+     {
+        score = 0
+        com_score = 1
+    }
+ }
+ else if(playerSelection == 'scissor')
+ {
+     if(computerSelection == 'paper')
+     {
+        score = 1
+        com_score = 0
+    }
+     else if(computerSelection == 'rock')
+     {
+        score = 0
+        com_score = 1
+     }
+ }
+ else if(playerSelection == 'paper')
+ {
+     if(computerSelection == 'rock')
+     {
+        score = 1
+        com_score = 0
+     }
+     else if(computerSelection == 'scissor')
+     {
+        score = 0
+        com_score = 1
+     }
+ }
 
-  return score 
+
+  return [score , com_score] 
 
 }
 
@@ -32,25 +69,29 @@ function computerPlay()
 }
 
 
-const computerSelection = computerPlay();
-
-
-const input1 = require('readline');
-var prompts = input1.createInterface(process.stdin,process.stdout);
-
 do{
+    const computerSelection = computerPlay();
 
-prompts.question("Enter your weapon : " , function(playerSelection) 
-{
+    playerSelection = readline.question("Enter your weapon : ");
 
-    score = playRound(playerSelection, computerSelection );
-    final_score = final_score + score
+    console.log(playerSelection +" "+ computerSelection)
+    let scores = playRound(playerSelection, computerSelection );
 
-});
+    computer_score = computer_score + scores[0]
+    player_score = player_score + scores[1]
 
+    console.log(computer_score)
+    console.log(player_score)
 
-console.log(final_score)
+    if(computer_score == 5 || player_score == 5)
+        b = true
+      
 
+}while(b == false)
 
-}while((final_score <= 5))
+if(player_score == 5)
+console.log("YOU DEFEAT THE COMPUTER")
+else
+console.log("COMPUTER DEFEATS YOU")
+
 
